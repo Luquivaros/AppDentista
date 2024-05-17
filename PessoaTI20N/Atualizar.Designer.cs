82077f90884aca46e473d285aff2dcfc924abd60
ALLAN SOBRAL DA SILVA <allan.ssilva4@senacsp.edu.br>
2024-05-16 22:06:21 -0300

    Primeiro Commit

---
+++ PessoaTI20N/Atualizar.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace PessoaTI20N
+{
+    partial class Atualizar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.label1.Location = new System.Drawing.Point(70, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Atualizar Dados";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(43, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Campo: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox1.Location = new System.Drawing.Point(136, 139);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 30);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox2.Location = new System.Drawing.Point(136, 185);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(180, 30);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(43, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Dado:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.Location = new System.Drawing.Point(58, 269);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 44);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Atualizar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button2.Location = new System.Drawing.Point(182, 269);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 44);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(43, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "CPF: ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox3.Location = new System.Drawing.Point(136, 98);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(180, 30);
+            this.textBox3.TabIndex = 8;
+            // 
+            // Atualizar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 374);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Atualizar";
+            this.Text = "Atualizar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
---
+++ PessoaTI20N/DAO.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient; //Importar o MySQL
+
+namespace PessoaTI20N
+{
+    class DAO
+    {
+        public MySqlConnection conexao;
+        public long[] cpf;
+        public string[] nome;
+        public string[] telefone;
+        public string[] endereco;
+        public int i;
+        public int contador;
+
+        public DAO()
+        {
+            conexao = new MySqlConnection("server=localhost;Database=empresaTI20N;Uid=root;password=");
+            try
+            {
+                conexao.Open();//Abrir a conexão
+                MessageBox.Show("Conectado!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Algo deu errado!\n\n" + ex);
+            }
+        }//fim do construtor
+
+        public string Inserir(long cpf, string nome, string telefone, string endereco)
+        {
+            string inserir = $"Insert into pessoa(cpf, nome, telefone, endereco) values" +
+                $"('{cpf}','{nome}','{telefone}','{endereco}')";
+
+            MySqlCommand sql = new MySqlCommand(inserir, conexao);
+            string resultado = sql.ExecuteNonQuery() + " Executado!";
+            return resultado;
+        }//fim do método
+
+        public void PreencherVetor()
+        {
+            string query = "select * from pessoa";
+
+            //Instanciar
+            this.cpf = new long[100];
+            this.nome = new string[100];
+            this.telefone = new string[100];
+            this.endereco = new string[100];
+
+            //Fazer o comando de seleção do banco
+            MySqlCommand sql = new MySqlCommand(query, conexao);
+            //Leitor do banco
+            MySqlDataReader leitura = sql.ExecuteReader();
+
+            i = 0;
+            contador = 0;
+            while (leitura.Read())
+            {
+                cpf[i]      = Convert.ToInt64(leitura["cpf"]);
+                nome[i]     = leitura["nome"] + "";
+                telefone[i] = leitura["telefone"] + "";
+                endereco[i] = leitura["endereco"] + "";
+                i++;//Percorrer o vetor
+                contador++;//Contar quantos dados eu tenho
+            }//fim do while
+
+            //Encerro a comunicação com o software
+            leitura.Close();
+        }//fim do preencher
+
+        //Criar o método para retornar o contador
+        public int QuantidadeDados()
+        {
+            return contador;
+        }//fim do quantidade de dados
+
+        public string Atualizar(long cpf, string nomeTabela, string campo, string dado)
+        {
+            string query = $"update {nomeTabela} set {campo} = '{dado}' where cpf = '{cpf}'";
+            MySqlCommand sql = new MySqlCommand(query, conexao);
+            string resultado = sql.ExecuteNonQuery() + " Atualizado!";
+            return resultado;
+        }//fim do método
+
+        public string Excluir(long cpf, string nomeTabela)
+        {
+            string query = $"delete from {nomeTabela} where CPF = '{cpf}'";
+            MySqlCommand sql = new MySqlCommand(query, conexao);
+            string resultado = sql.ExecuteNonQuery() + " Excluído!";
+            return resultado;
+        }//fim do excluir
+    }//fim da classe
+}//fim do projeto
